31cdc6202aff71abe17785e394cf4d95c408c68d
Anirudh Bharadwaj <67722222+anirudhb123@users.noreply.github.com>
2025-04-16 16:11:55 -0400

    Update schema.js

--- schema.js
+++ schema.js
@@ -11,4 +11,4 @@ const example = new mongoose.Schema({
 });
 
 // Modify the 3rd parameter to specify which MongoDB collection to use
-module.exports = mongoose.model("Example", example, "structure_perturbations");
+module.exports = mongoose.model("Example", example, "jargon_perturbations");
