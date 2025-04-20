d24192b1f67840949eb36e3611bf41c3692078d9
Anirudh Bharadwaj <67722222+anirudhb123@users.noreply.github.com>
2025-04-20 19:52:53 -0400

    update submission code

--- frontend/src/pages/SubmissionPage.js
+++ frontend/src/pages/SubmissionPage.js
@@ -10,7 +10,7 @@ const SubmissionPage = () => {
                         <br/>
                         <p> Thank you for participating in this study. </p>
                         <p> Your completion code is: </p>
-                        <h2> C1CJQT0C </h2>
+                        <h2> C1FC9LNU </h2>
                     </Card.Text>
                 </Card.Body>
             </Card>
